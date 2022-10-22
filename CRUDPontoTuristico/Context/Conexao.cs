2f2c52ac60fd98a756082e085904c56eaf6c37eb
julyorybas <80602260+julyorybas@users.noreply.github.com>
2022-10-22 16:54:01 -0300

    first commit

---
+++ CRUDPontoTuristico/Context/Conexao.cs
@@ -0,0 +1,14 @@
+﻿using CRUDPontoTuristico.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDPontoTuristico.Context
+{
+    public class Conexao : DbContext
+    {
+        public Conexao(DbContextOptions<DbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<PontoTuristico> PontoTuristico { get; set; }
+    }
+}
---
+++ CRUDPontoTuristico/Models/PontoTuristico.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUDPontoTuristico.Models
+{
+    [Table("PontoTuristo")]
+    public class PontoTuristico
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome{ get; set; }
+
+        [Column("UF")]
+        [Display(Name = "UF")]
+        public string UF { get; set; }
+
+        [Column("Cidade")]
+        [Display(Name = "Cidade")]
+        public string Cidade { get; set; }
+
+        [Column("Referencia")]
+        [Display(Name = "Referencia")]
+        public string Referencia { get; set; }
+
+        [Column("Descricao")]
+        [Display(Name = "Descricao")]
+        public string Descricao { get; set; }
+    }
+}
